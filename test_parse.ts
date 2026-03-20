616166df458e0175f6d3e1950e1a48636fb50fe8
s0fractal <sergey.glova@gmail.com>
2026-03-20 21:37:37 +0200

    Refactor: relocated native WGSL shader compilation verifiers into the tools pipeline, discarding obsolete AST TOON probes.

--- test_parse.ts
+++
@@ -1,4 +0,0 @@
-import { parseTissueFromMarkdown } from "./src/quine.ts";
-const t = await parseTissueFromMarkdown("./I.md");
-console.log("Atomic Pulse Expr:", t.atomic_pulse.expr);
-console.log("Fast Abs Expr:", t.fast_abs.expr);
